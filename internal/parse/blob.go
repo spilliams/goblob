00e3efe13c40150f53e58827ac9a07c4b460430f
Spencer Williams <s@spencerenglish.com>
2020-07-11 22:21:26 -0700

    closer to working

--- internal/parse/blob.go
+++ internal/parse/blob.go
@@ -16,97 +16,137 @@ func NewBlobParser() *BlobParser {
 	return &BlobParser{}
 }
 
-// ParseString parses a string-encoded blob
+// Parse parses a string-encoded blob
 // Empty arrays will parse as an empty map[string]interface{}
 func (bp *BlobParser) Parse(s string) error {
-	split := strings.Split(s, ":")
-	switch split[0] {
-	case "a":
-		if len(split) < 3 {
-			return fmt.Errorf("input indicated array but it had %d colon-separated elements (expected >=3)", len(split))
-		}
-		if len(split[2]) < 2 {
-			return fmt.Errorf("input indicated array but third colon arg was unexpectedly short")
-		}
-		runes := []rune(split[2])
-		arrayType := string(runes[1])
-		switch arrayType {
-		case "}":
-			// empty array input
-			bp.parsedObj = map[string]interface{}{}
-			return nil
-		case "i":
-			return bp.parseList(s)
-		case "s":
-			return bp.parseMap(s)
-		default:
-			return fmt.Errorf("input indicated array but array type is unrecognized (%c)", split[2][1])
-		}
-	case "s":
-		return bp.parseString(s)
-	case "i":
-		return bp.parseInteger(s)
-	case "b":
-		return bp.parseBoolean(s)
-	default:
-		return fmt.Errorf("unknown data type '%c' in input", s[0])
-	}
-}
-
-func (bp *BlobParser) parseList(s string) error {
-	// TODO implement me
-	return fmt.Errorf("not implemented list")
-}
-
-func (bp *BlobParser) parseMap(s string) error {
-	// TODO implement me
-	return fmt.Errorf("not implemented map")
-}
-
-// ex: s:4:"abcd";
-func (bp *BlobParser) parseString(s string) error {
-	split := strings.Split(s, ":")
-	if len(split) != 3 {
-		return fmt.Errorf("input indicated string but it had the wrong number of colon args (expected 3, actual %d)", len(split))
-	}
-	runes := []rune(split[2])
-	bp.parsedObj = string(runes[1 : len(runes)-2])
-	return nil
-}
-
-func (bp *BlobParser) parseInteger(s string) error {
-	split := strings.Split(s, ":")
-	if len(split) != 2 {
-		return fmt.Errorf("input indicated integer but it had the wrong number of colon args (expected 2, actual %d)", len(split))
-	}
 	var err error
-	runes := []rune(split[1])
-	bp.parsedObj, err = strconv.Atoi(string(runes[0 : len(runes)-1]))
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (bp *BlobParser) parseBoolean(s string) error {
-	split := strings.Split(s, ":")
-	if len(split) != 2 {
-		return fmt.Errorf("input indicated boolean but it had the wrong number of colon args (expected 2, actuald %d)", len(split))
-	}
-	runes := []rune(split[1])
-	value := string(runes[0])
-	switch value {
-	case "0":
-		bp.parsedObj = false
-		return nil
-	case "1":
-		bp.parsedObj = true
-		return nil
-	}
-	return fmt.Errorf("unrecognized boolean value (expect 0 or 1, actual %v)", split[1])
+	bp.parsedObj, err = parse(s)
+	return err
 }
 
 // ParsedObject returns the receiver's most recently parsed object
 func (bp *BlobParser) ParsedObject() interface{} {
 	return bp.parsedObj
+}
+
+func parse(s string) (interface{}, error) {
+	t, err := makeToken(s)
+	if err != nil {
+		return nil, err
+	}
+	switch t.key {
+	case "a":
+		arrayType := string(t.value[1])
+		switch arrayType {
+		case "}":
+			// empty array input
+			return map[string]interface{}{}, nil
+		case "i":
+			return parseList(t)
+		case "s":
+			return parseMap(t)
+		default:
+			return nil, fmt.Errorf("input indicated array but array type is unrecognized (%v)", arrayType)
+		}
+	case "s":
+		return parseString(t)
+	case "i":
+		return parseInteger(t)
+	case "b":
+		return parseBoolean(t)
+	default:
+		return nil, fmt.Errorf("unknown data type '%c' in input", s[0])
+	}
+}
+
+const (
+	tokenArrayKey  = "a"
+	tokenBoolKey   = "b"
+	tokenIntKey    = "i"
+	tokenStringKey = "s"
+)
+
+type token struct {
+	key    string
+	length int
+	value  string
+}
+
+func makeToken(s string) (token, error) {
+	split := strings.Split(s, ":")
+	if len(split) < 2 || len(split) > 3 {
+		return token{}, fmt.Errorf("input had only %d colon args (expected 2 or 3)", len(split))
+	}
+	runes := []rune(split[len(split)-1])
+	value := string(runes[0 : len(runes)-2])
+	t := token{
+		key:   split[0],
+		value: value,
+	}
+	if len(split) == 3 {
+		length, err := strconv.Atoi(split[1])
+		if err != nil {
+			return t, err
+		}
+		t.length = length
+	}
+
+	return t, nil
+}
+
+// ex: a:N:{i:0;...}
+func parseList(t token) ([]interface{}, error) {
+	value := string(t.value[1 : len(t.value)-1])
+	split := strings.Split(value, ";")
+	list := []interface{}{}
+	for i, v := range split {
+		if i%2 == 0 {
+			continue
+		}
+		parsedVal, err := parse(v)
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, parsedVal)
+	}
+	return list, nil
+}
+
+// ex: a:N:{s:3:"key";...}
+func parseMap(t token) (map[string]interface{}, error) {
+	value := string(t.value[1 : len(t.value)-1])
+	split := strings.Split(value, ";")
+	dict := map[string]interface{}{}
+	for i := 0; i < len(split); i += 2 {
+		keyToken, err := makeToken(split[i])
+		if err != nil {
+			return nil, err
+		}
+		if keyToken.key != tokenStringKey {
+			return nil, fmt.Errorf("map key was type %v (expected %v)", keyToken.key, tokenStringKey)
+		}
+		val, err := parse(split[i+1])
+		if err != nil {
+			return nil, err
+		}
+		dict[string(keyToken.value)] = val
+	}
+	return dict, nil
+}
+
+// ex: s:4:"abcd"
+func parseString(t token) (string, error) {
+	s := t.value[1 : len(t.value)-1]
+	if t.length != len(s) {
+		return "", fmt.Errorf("string is the wrong length (expected %v, actual %v)", t.length, len(s))
+	}
+	return s, nil
+}
+
+func parseInteger(t token) (int, error) {
+	return strconv.Atoi(t.value)
+}
+
+func parseBoolean(t token) (bool, error) {
+	return strconv.ParseBool(t.value)
 }
--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/spilliams/goblob/internal/parse"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/spilliams/goblob/internal/parse"
+)
 
 func main() {
 	// look for string arg
@@ -8,7 +14,15 @@ func main() {
 
 	// parse string
 	p := parse.NewBlobParser()
-	// p.Parse()
+	err := p.Parse("s:4:\"1234\";")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// output in json
+	out, err := json.Marshal(p.ParsedObject())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(out))
 }
